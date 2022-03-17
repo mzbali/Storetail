4070f9c866d1bf9546fff5f729e5093eff549829
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-03-17 06:06:29 +0600

    main entry of the project lot of services for dependency injection and middlewares

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); // connectionString of postgres database
+builder.Services.AddDbContext<StoreContext>(opt =>
+    opt.UseNpgsql(connectionString) // connection with postgres
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection(); // No redirection to https, since in devoloper mode
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
