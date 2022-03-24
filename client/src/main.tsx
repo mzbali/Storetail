cc4671ce276971a483d1a38f872f32a567c25d98
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-03-25 01:32:42 +0600

    index file mounted to html

---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/layout/styles.css';
+import App from './app/layout/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
