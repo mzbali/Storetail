8dd3b3c443e42a1e92d4ea7237a517cff9ca6fb7
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-03-25 01:36:06 +0600

    loops through product and shows in a card

---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,18 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+import { Product } from '../../app/models/product';
+import { ProductCard } from './ProductCard';
+
+interface ProductsListProps {
+  products: Product[];
+}
+
+export const ProductList: React.FC<ProductsListProps> = ({ products }) => {
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <ProductCard product={product} />
+      ))}
+    </Grid>
+  );
+};
