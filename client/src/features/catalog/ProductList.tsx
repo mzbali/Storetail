736f168b66fb14f432305c8db2ae0f067d18d065
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-04-01 03:28:22 +0600

    providing key to every ProductCard

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -11,7 +11,7 @@ export const ProductList: React.FC<ProductsListProps> = ({ products }) => {
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-        <ProductCard product={product} />
+        <ProductCard key={product.id} product={product} />
       ))}
     </Grid>
   );
