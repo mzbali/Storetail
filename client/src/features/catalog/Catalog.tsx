b9d0db2661a1b4ecb83c5ab268bbbc26dfc7d68e
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-04-01 03:13:42 +0600

    using custom axios REST resource function

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import agent from '../../app/api/agent';
 import { Product } from '../../app/models/product';
 import { ProductList } from './ProductList';
 
 export const Catalog: React.FC = ({}) => {
   const [products, setProducts] = useState<Product[]>([]);
   useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    agent.Catalog.catalog()
+      .then((data) => setProducts(data))
+      .catch((error) => console.log(error));
   }, []);
   return <ProductList products={products} />;
 };
