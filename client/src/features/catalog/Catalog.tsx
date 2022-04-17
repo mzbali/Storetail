e84290cdccb4035c7a781b171a14874e7b025072
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-04-17 06:50:16 +0600

    using loading component in catalog page

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import agent from '../../app/api/agent';
+import { LoadingComponent } from '../../app/layout/LoadingComponent';
 import { Product } from '../../app/models/product';
 import { ProductList } from './ProductList';
 
 export const Catalog: React.FC = ({}) => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     agent.Catalog.catalog()
       .then((data) => setProducts(data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) {
+    return <LoadingComponent />;
+  }
   return <ProductList products={products} />;
 };
