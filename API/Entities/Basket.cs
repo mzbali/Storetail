d943e9bda9b7c4d00d266e4a8c5c637fc137260a
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-06-21 03:28:23 +0600

    Bakset Enitity with BuyerId and List of BasketItem

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,31 @@
+namespace API.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public int BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+
+        // we'll send the productId then find the Product in controller, then add the item to Basket
+        // so no need to find it in AddItem
+        public void AddItem(Product product, int quantity)
+        {
+            if (Items.All(item => item.Id != product.Id))
+            {
+                Items.Add(new BasketItem { Quantity = quantity, Product = product });
+            }
+            var existingItem = Items.FirstOrDefault(item => item.Id == product.Id);
+
+            if (existingItem != null) existingItem.Quantity += quantity;
+        }
+
+        public void removeItem(int productId, int quantity)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+            if (item == null) return;
+            item.Quantity -= quantity;
+            if (item.Quantity == 0) Items.Remove(item);
+        }
+
+    }
+}
