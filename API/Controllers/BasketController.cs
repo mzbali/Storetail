88078aa8d1cddcf60080be3e8dea157a2b7d8437
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2022-06-21 03:39:26 +0600

    Basket Controller with getBasket for single user

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetBasket")]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(item => item.BuyerId.ToString() == Request.Cookies["buyerId"]);
+
+            if (basket == null) return NotFound();
+
+            return basket;
+        }
+
+    }
+}
