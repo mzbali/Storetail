8e8285d8fbfb33b7b215f23ddf1c901ee2951807
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-05-25 01:58:15 +0100

    added basketpage table

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'react';
 import { Basket } from '../../app/models/basket';
 import { LoadingComponent } from '../../app/layout/LoadingComponent';
-import { Typography } from '@mui/material';
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete'; // <- DeleteIcon import
 import agent from '../../app/api/agent';
 
 export const BasketPage: React.FC = () => {
@@ -23,5 +34,43 @@ export const BasketPage: React.FC = () => {
 
   if (!basket) return <Typography variant="h3">There is no basket</Typography>;
 
-  return <h1>Buyer ID= {basket?.buyerId}</h1>;
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map((item) => (
+            <TableRow
+              key={item.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">
+                {(item.price / 100).toFixed(2)}
+              </TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">
+                {((item.price / 100) * item.quantity).toFixed(2)}
+              </TableCell>
+              <TableCell align="right">
+                <IconButton aria-label="delete" size="large">
+                  <DeleteIcon fontSize="inherit" color="error" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
