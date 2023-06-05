d2bf50a47861fd22cfd3b2cb5838f633283d5b35
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-06-05 23:54:49 +0100

    providing basket through context api to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,16 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { ContextProvider } from './app/context/StoreContext';
 
 export const history = createBrowserHistory({ window });
 
 ReactDOM.render(
   <React.StrictMode>
     <HistoryRouter history={history}>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </HistoryRouter>
   </React.StrictMode>,
   document.getElementById('root')
