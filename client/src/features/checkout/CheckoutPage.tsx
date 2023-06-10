bd4abd2d2fd030492b9904e082723ccf1ddb3483
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-06-10 04:32:21 +0100

    Checkout page for paying

---
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+
+const CheckoutPage = () => {
+  return (
+    <Typography variant="h3">
+      This page should be only accessiable when logged in.
+    </Typography>
+  );
+};
+export default CheckoutPage;
