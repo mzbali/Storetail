2d003a14f5122dca61da215ac4e8dd25a3b1ea90
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-06-15 15:46:56 +0100

    providing redux store to the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
 import { ContextProvider } from './app/context/StoreContext';
 
 export const history = createBrowserHistory({ window });
@@ -11,9 +13,11 @@ export const history = createBrowserHistory({ window });
 ReactDOM.render(
   <React.StrictMode>
     <HistoryRouter history={history}>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
+      <Provider store={store}>
+        <ContextProvider>
+          <App />
+        </ContextProvider>
+      </Provider>
     </HistoryRouter>
   </React.StrictMode>,
   document.getElementById('root')
