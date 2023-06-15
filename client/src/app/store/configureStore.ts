7547d383e726600e8040291edb2ecb64bb0a88c9
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-06-15 15:45:42 +0100

    configuring redux store, defining data types

---
+++ client/src/app/store/configureStore.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { counterSlice } from '../../features/contact/counterReducer';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+// Basically taking the type from redux and putting to react.
+
+type Rootstate = ReturnType<typeof store.getState>; //will have the type of the object that represents the entire Redux state.
+export const useAppSelector: TypedUseSelectorHook<Rootstate> = useSelector; // selecto things that actually exist in store
+
+type AppDispatch = typeof store.dispatch; // represents the dispatch function of the Redux store
+export const useAppDispatch = () => useDispatch<AppDispatch>();
