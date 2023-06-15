dfeb7b4da1db092c994e2551f1c0244193b1dabf
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-06-15 15:47:45 +0100

    use redex to control the counter value

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,7 +1,40 @@
+import { Button, ButtonGroup, Typography } from '@mui/material';
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import { decrement, increment } from './counterReducer';
 
 interface ContactPageProps {}
 
 export const ContactPage: React.FC<ContactPageProps> = ({}) => {
-  return <h1>Contact Page</h1>;
+  const dispatch = useAppDispatch();
+  const counter = useAppSelector((state) => state.counter);
+  return (
+    <>
+      <Typography variant="h3">Contact Page</Typography>
+      <Typography variant="h5">{counter.count}</Typography>
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => dispatch(increment(1))}
+        >
+          Increment
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => dispatch(decrement(1))}
+        >
+          Decrement
+        </Button>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() => dispatch(increment(5))}
+        >
+          Increase +5
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 };
