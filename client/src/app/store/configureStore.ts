f48bea377bf558f9dd8bc42def5ad0f5a9ea63b5
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-09-15 16:38:41 +0100

    added catalogSlice

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -2,17 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from '../../features/contact/counterReducer';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import basketSlice from '../../features/basket/basketSlice';
+import { catalogSlice } from '../../features/catalog/catalogSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     basket: basketSlice.reducer,
+    catalog: catalogSlice.reducer,
   },
 });
 
 // Basically taking the type from redux and putting to react.
 
-type Rootstate = ReturnType<typeof store.getState>; //will have the type of the object that represents the entire Redux state.
+export type Rootstate = ReturnType<typeof store.getState>; //will have the type of the object that represents the entire Redux state.
 export const useAppSelector: TypedUseSelectorHook<Rootstate> = useSelector; // selecto things that actually exist in store
 
 type AppDispatch = typeof store.dispatch; // represents the dispatch function of the Redux store
