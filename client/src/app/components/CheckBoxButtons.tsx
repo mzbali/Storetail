23910a7c82416991573cc653e6ca49588760f32b
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-09-21 14:43:32 +0100

    Checkbox component filter based on brands or types

---
+++ client/src/app/components/CheckBoxButtons.tsx
@@ -0,0 +1,52 @@
+import {
+  FormGroup,
+  FormLabel,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import React, { useState } from "react";
+
+interface CheckBoxButtonsProps {
+  checkBoxLabel: string;
+  items: string[];
+  checked?: string[];
+  onChange: (items: string[]) => void;
+}
+
+const CheckBoxButtons: React.FC<CheckBoxButtonsProps> = ({
+  items,
+  checked,
+  onChange,
+  checkBoxLabel,
+}) => {
+  const [checkedItems, setCheckedItems] = useState(checked || []);
+
+  const handleChecked = (value: string) => {
+    const currentIndex = checkedItems.findIndex((item) => item === value);
+    let newChecked: string[] = [];
+    if (currentIndex === -1) newChecked = [...checkedItems, value];
+    else newChecked.filter((item) => item !== value);
+    setCheckedItems(newChecked);
+    onChange(newChecked);
+  };
+  return (
+    <FormGroup>
+      <FormLabel>{checkBoxLabel}</FormLabel>
+      {items.map((item) => (
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checkedItems.indexOf(item) !== -1}
+              onChange={() => {
+                handleChecked(item);
+              }}
+            />
+          }
+          label={item}
+        />
+      ))}
+    </FormGroup>
+  );
+};
+
+export default CheckBoxButtons;
