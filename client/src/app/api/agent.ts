498705aede5ec00a9be145f4e668fa13b87bc725
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-09-21 14:23:15 +0100

    intregated queryParams request to API

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,8 +1,8 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { toast } from 'react-toastify';
-import { history } from '../../main';
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import { history } from "../../main";
 
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.baseURL = "http://localhost:5000/api/";
 axios.defaults.withCredentials = true; // to allow cookies to be sent to server.
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -32,10 +32,10 @@ axios.interceptors.response.use(
         toast.error(response.statusText);
         break;
       case 404:
-        history.push('/not-found');
+        history.push("/not-found");
         break;
       case 500:
-        history.push('/server-error', { data: response.data });
+        history.push("/server-error", { data: response.data });
         break;
       default:
         break;
@@ -45,27 +45,29 @@ axios.interceptors.response.use(
 );
 
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
+  get: (url: string, params?: URLSearchParams) =>
+    axios.get(url, { params }).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
   put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Catalog = {
-  catalog: () => requests.get('products'),
+  catalog: (params: URLSearchParams) => requests.get("products", params),
   details: (id: number) => requests.get(`products/${id}`),
+  fetchFilters: () => requests.get("products/filters"),
 };
 
 const Buggy = {
-  get400Error: () => requests.get('buggy/bad-request'),
-  get401Error: () => requests.get('buggy/unauthorised'),
-  get404Error: () => requests.get('buggy/not-found'),
-  get500Error: () => requests.get('buggy/server-error'),
-  getValidationError: () => requests.get('buggy/validation-error'),
+  get400Error: () => requests.get("buggy/bad-request"),
+  get401Error: () => requests.get("buggy/unauthorised"),
+  get404Error: () => requests.get("buggy/not-found"),
+  get500Error: () => requests.get("buggy/server-error"),
+  getValidationError: () => requests.get("buggy/validation-error"),
 };
 
 const Basket = {
-  get: () => requests.get('basket'),
+  get: () => requests.get("basket"),
   addItems: (productId: number, quantity = 1) =>
     requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
   removeItems: (productId: number, quantity = 1) =>
