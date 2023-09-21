d0d3bae1452d1c463fc7f861e68d32e856a2964a
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-09-21 14:44:26 +0100

    updated catalog page with search, sort and filter

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,75 @@
-import React, { useEffect } from 'react';
-import { LoadingComponent } from '../../app/layout/LoadingComponent';
-import { ProductList } from './ProductList';
-import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
-import { fetchProductsAsync, productSelectors } from './catalogSlice';
+import React, { useEffect } from "react";
+import { LoadingComponent } from "../../app/layout/LoadingComponent";
+import { ProductList } from "./ProductList";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { Grid, Paper } from "@mui/material";
+import {
+  fetchFiltersAsync,
+  fetchProductsAsync,
+  productSelectors,
+  setProductParams,
+} from "./catalogSlice";
+import SearchProduct from "../../app/components/SearchProduct";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
+import CheckBoxButtons from "../../app/components/CheckBoxButtons";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "price", label: "Price - Low to High" },
+  { value: "priceDesc", label: "Price - High to Low" },
+];
 
 export const Catalog: React.FC = ({}) => {
   const products = useAppSelector(productSelectors.selectAll);
-  const { status, productsLoaded } = useAppSelector((state) => state.catalog);
+  const { productsLoaded, filtersFetched, brands, types, productParams } =
+    useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
-  }, [productsLoaded, fetchProductsAsync]);
+  }, [dispatch, productsLoaded, fetchProductsAsync]);
 
-  if (status.includes('pending')) {
+  useEffect(() => {
+    if (!filtersFetched) dispatch(fetchFiltersAsync());
+  }, [filtersFetched, fetchFiltersAsync, dispatch]);
+
+  if (!productsLoaded) {
     return <LoadingComponent />;
   }
-  return <ProductList products={products} />;
+  return (
+    <Grid container columnSpacing={2}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <SearchProduct />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <RadioButtonGroup
+            options={sortOptions}
+            onChange={(e: any) =>
+              dispatch(setProductParams({ orderBy: e.target.value }))
+            }
+          />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <CheckBoxButtons
+            items={brands}
+            checked={productParams.brands}
+            onChange={(items) => dispatch(setProductParams({ brands: items }))}
+            checkBoxLabel="Brands"
+          />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <CheckBoxButtons
+            items={types}
+            checked={productParams.types}
+            onChange={(items) => dispatch(setProductParams({ types: items }))}
+            checkBoxLabel="Types"
+          />
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+    </Grid>
+  );
 };
