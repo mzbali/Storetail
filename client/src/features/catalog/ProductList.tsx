9c4f316dbce1971d7a818d27caf21ba66c5fa5c4
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-09-25 22:06:45 +0100

    Putting loading skeleton when no product

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -2,16 +2,27 @@ import { Grid } from "@mui/material";
 import React from "react";
 import { Product } from "../../app/models/product";
 import { ProductCard } from "./ProductCard";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface ProductsListProps {
   products: Product[];
+  productsLoaded: boolean;
 }
 
-export const ProductList: React.FC<ProductsListProps> = ({ products }) => {
+export const ProductList: React.FC<ProductsListProps> = ({
+  products,
+  productsLoaded,
+}) => {
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <Grid item xs={4} key={product.id}>
+          {productsLoaded ? (
+            <ProductCard product={product} />
+          ) : (
+            <ProductCardSkeleton />
+          )}
+        </Grid>
       ))}
     </Grid>
   );
