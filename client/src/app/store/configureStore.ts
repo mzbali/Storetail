b3e7c0c468444515a836fe6125604fb977328a0b
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-10-09 22:31:42 +0100

    Added accountReducer to the redux store

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,15 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from '../../features/contact/counterReducer';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import {counterSlice} from '../../features/contact/counterReducer';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import basketSlice from '../../features/basket/basketSlice';
-import { catalogSlice } from '../../features/catalog/catalogSlice';
+import {catalogSlice} from '../../features/catalog/catalogSlice';
+import {accountSlice} from "../../features/account/accountSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer,
-    basket: basketSlice.reducer,
-    catalog: catalogSlice.reducer,
-  },
+    reducer: {
+        counter: counterSlice.reducer,
+        basket: basketSlice.reducer,
+        catalog: catalogSlice.reducer,
+        account: accountSlice.reducer
+    },
 });
 
 // Basically taking the type from redux and putting to react.
