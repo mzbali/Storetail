f709ebbbd9c25f08b6f7909d3129b95545169c3a
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-10-16 23:07:24 +0100

    using RouterProvider to provide which page to render

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { Provider } from 'react-redux';
-import { store } from './app/store/configureStore';
-import { ContextProvider } from './app/context/StoreContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app/layout/styles.css";
+import {Provider} from "react-redux";
+import {store} from "./app/store/configureStore";
+import {RouterProvider} from "react-router-dom";
+import router from "./app/router/Routes";
 
-export const history = createBrowserHistory({ window });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HistoryRouter history={history}>
-      <Provider store={store}>
-        <ContextProvider>
-          <App />
-        </ContextProvider>
-      </Provider>
-    </HistoryRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
