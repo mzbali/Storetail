660ec8aa6aca358b7bd3533f3d0eb4580bea7a5f
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-10-16 23:18:06 +0100

    using outlet to render child element

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,23 +1,12 @@
 import {useCallback, useEffect, useState} from "react";
 import {Container, createTheme, CssBaseline, ThemeProvider,} from "@mui/material";
-import {Catalog} from "../../features/catalog/Catalog";
 import {Header} from "./Header";
-import {Route, Routes} from "react-router-dom";
-import {HomePage} from "../../features/home/HomePage";
-import {AboutPage} from "../../features/about/AboutPage";
-import {ContactPage} from "../../features/contact/ContactPage";
-import {ProductDetails} from "../../features/catalog/ProductDetails";
+import {Outlet} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {ServerError} from "../errors/ServerError";
-import {NotFound} from "../errors/NotFound";
-import {BasketPage} from "../../features/basket/BasketPage";
 import {LoadingComponent} from "./LoadingComponent";
-import CheckoutPage from "../../features/checkout/CheckoutPage";
 import {useAppDispatch} from "../store/configureStore";
 import {fetchBasketAsync} from "../../features/basket/basketSlice";
-import Login from "../../features/account/Login";
-import Register from "../../features/account/Register";
 import {fetchCurrentUser} from "../../features/account/accountSlice";
 
 const App = () => {
@@ -62,19 +51,7 @@ const App = () => {
             <CssBaseline/>
             <Header onSwitchClick={toggleThemeHandler} mode={darkMode}/>
             <Container>
-                <Routes>
-                    <Route path="/basket" element={<BasketPage/>}/>
-                    <Route path="/catalog" element={<Catalog/>}/>
-                    <Route path="/about" element={<AboutPage/>}/>
-                    <Route path="/contact" element={<ContactPage/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/products/:id" element={<ProductDetails/>}/>
-                    <Route path="/server-error" element={<ServerError/>}/>
-                    <Route path="/not-found" element={<NotFound/>}/>
-                    <Route path="/checkout" element={<CheckoutPage/>}/>
-                    <Route path="/" element={<HomePage/>}/>
-                </Routes>
+                <Outlet/>
             </Container>
         </ThemeProvider>
     );
