eaa3deec0dd67427fc86cd47e20147dc6bc819d9
Mahmud Zaman Bali <mahmud.bali@tuta.io>
2023-10-16 23:11:34 +0100

    using location and navigate to go programmatically different pages

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,42 +1,47 @@
-import Avatar from '@mui/material/Avatar';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import {Link} from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {Paper} from "@mui/material";
 import {FieldValues, useForm} from "react-hook-form";
 import {LoadingButton} from "@mui/lab";
 import {loginUserAsync} from "./accountSlice";
-import {history} from "../../main";
 import {useAppDispatch} from "../../app/store/configureStore";
 
 const Login = () => {
     const dispatch = useAppDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
     const {
         register,
         handleSubmit,
         formState: {isSubmitting, errors, isValid}
     } = useForm({mode: "all"});
     const submitForm = async (data: FieldValues) => {
-        dispatch(loginUserAsync(data));
-        history.push('/catalog');
+        try {
+            await dispatch(loginUserAsync(data));
+            navigate(location.state?.from.pathname || "/catalog");
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
         <Container component={Paper} maxWidth="xs" sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             padding: 3
         }}>
 
-            <Avatar sx={{m: 1, backgroundColor: 'secondary.main'}}>
+            <Avatar sx={{m: 1, backgroundColor: "secondary.main"}}>
                 <LockOutlinedIcon/>
             </Avatar>
             <Typography component="h1" variant="h5">
