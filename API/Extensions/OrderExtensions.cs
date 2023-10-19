bb669649bba0e2c23e2d9e5210faafdfc4f2880a
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-10-19 18:27:52 +0100

    projecting Order to OrderDto

---
+++ API/Extensions/OrderExtensions.cs
@@ -0,0 +1,32 @@
+using API.DTOs;
+using API.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IQueryable<OrderDto> ProjectOrderToOrderDto(this IQueryable<Order> order)
+        {
+            return order.Select(o => new OrderDto
+            {
+                Id = o.Id,
+                BuyerId = o.BuyerId,
+                Address = o.ShippingAddress,
+                OrderItems = o.OrderItems.Select(item => new OrderItemDto
+                {
+                    ProductId = item.Product.ProductId,
+                    Name = item.Product.Name,
+                    PictureUrl = item.Product.PictureUrl,
+                    Quantity = item.Quantity,
+                    Price = item.Price
+                }).ToList(),
+                OrderDate = o.OrderDate,
+                SubTotal = o.SubTotal,
+                OrderStatus = o.Status.ToString(),
+                DeliveryFee = o.DeliveryFee,
+                Total = o.GetTotal()
+            }).AsNoTracking();
+        }
+    }
+}
