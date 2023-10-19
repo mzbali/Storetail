88f471493cc9b1ebb4d8fbb2c07beaaec0f31d6e
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-10-19 23:10:12 +0100

    controlled form using useFormContext

---
+++ client/src/features/checkout/AddressForm.tsx
@@ -0,0 +1,40 @@
+import {Grid, Typography} from "@mui/material";
+import AppTextField from "../../app/components/AppTextField";
+import {useFormContext} from "react-hook-form";
+import AppCheckBox from "../../app/components/AppCheckBox";
+
+const AddressForm = () => {
+    const {control, formState} = useFormContext();
+    return (
+        <>
+            <Typography variant="h6" gutterBottom>
+                Shipping address
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12}>
+                    <AppTextField label="Full Name" name="fullName" control={control}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <AppTextField label="Address Line 1" name="address1" control={control}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <AppTextField label="Address Line 2" name="address2" control={control}/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextField label="City" name="city" control={control}/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextField label="State" name="state" control={control}/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <AppTextField label="Country" name="country" control={control}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <AppCheckBox label="Save this Address" name="saveAdress"/>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default AddressForm;
