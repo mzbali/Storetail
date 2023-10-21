384d036733997ac3bfdbee0fb7d99eefb6108197
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-10-21 16:02:19 +0100

    orderSlice to redux store

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,16 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {counterSlice} from '../../features/contact/counterReducer';
-import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
-import basketSlice from '../../features/basket/basketSlice';
-import {catalogSlice} from '../../features/catalog/catalogSlice';
+import {configureStore} from "@reduxjs/toolkit";
+import {counterSlice} from "../../features/contact/counterReducer";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import basketSlice from "../../features/basket/basketSlice";
+import {catalogSlice} from "../../features/catalog/catalogSlice";
 import {accountSlice} from "../../features/account/accountSlice";
+import checkoutSlice from "../../features/checkout/checkoutSlice";
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
         basket: basketSlice.reducer,
         catalog: catalogSlice.reducer,
-        account: accountSlice.reducer
+        account: accountSlice.reducer,
+        orders: checkoutSlice.reducer
     },
 });
 
