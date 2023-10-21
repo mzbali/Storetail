347fe1c4b824823d199412a0479f6d8eccf8d5a9
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-10-21 15:54:34 +0100

    server error page code clean up

--- client/src/app/errors/ServerError.tsx
+++ client/src/app/errors/ServerError.tsx
@@ -1,28 +1,26 @@
-import { Container, Typography, Paper, Divider } from '@mui/material';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { BtnToCatalog } from '../layout/button/BtnToCatalog';
+import {Container, Divider, Paper, Typography} from "@mui/material";
+import React from "react";
+import {useLocation} from "react-router-dom";
+import {BtnToCatalog} from "../layout/button/BtnToCatalog";
 
-interface ServerErrorProps {}
-
-export const ServerError: React.FC<ServerErrorProps> = ({}) => {
-  const { state }: any = useLocation();
-  return (
-    <Container component={Paper}>
-      {state?.data ? (
-        <>
-          <Typography gutterBottom variant="h5" color="red">
-            {state.data.title}
-          </Typography>
-          <Divider />
-          <Typography>
-            {state.data.detail || 'Internal Server Error'}
-          </Typography>
-        </>
-      ) : (
-        <Typography color="red">Server Error</Typography>
-      )}
-      <BtnToCatalog />
-    </Container>
-  );
+export const ServerError: React.FC = () => {
+    const {state}: any = useLocation();
+    return (
+        <Container component={Paper}>
+            {state?.data ? (
+                <>
+                    <Typography gutterBottom variant="h5" color="red">
+                        {state.data.title}
+                    </Typography>
+                    <Divider/>
+                    <Typography>
+                        {state.data.detail || "Internal Server Error"}
+                    </Typography>
+                </>
+            ) : (
+                <Typography color="red">Server Error</Typography>
+            )}
+            <BtnToCatalog/>
+        </Container>
+    );
 };
