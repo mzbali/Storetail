75ed5ad2ea8a56d5e3fdbf926f0f4a1b65e1f286
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-10-30 20:19:51 +0000

    webhook for stripe payment confirmation

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,27 +1,33 @@
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Extensions;
 using API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace API.Controllers
 {
     public class PaymentsController : BaseController
     {
+        private readonly IConfiguration _config;
         private readonly StoreContext _context;
         private readonly PaymentService _payment;
-        public PaymentsController(PaymentService payment, StoreContext context)
+        public PaymentsController(PaymentService payment, StoreContext context, IConfiguration config)
         {
             _payment = payment;
             _context = context;
-
+            _config = config;
         }
+
+        [Authorize]
         [HttpPost]
         public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent()
         {
             var basket = await _context.Baskets.RetrieveBasketWithItems(User.Identity?.Name).FirstOrDefaultAsync();
-            if (basket is null)
+            if (basket == null)
                 return BadRequest(new ProblemDetails
                 {
                     Title = "Basket does not exist for creating PI."
@@ -38,6 +44,32 @@ namespace API.Controllers
                     Title = "Problem updating basket for PI."
                 });
             return basket.MapToBasketDto();
+        }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility
+                .ConstructEvent(json, Request.Headers["Stripe-Signature"], _config["StripeSettings:WhSecret"]);
+            var charge = (Charge)stripeEvent.Data.Object;
+
+            var order = await _context.Orders.FirstOrDefaultAsync(o => o.PaymentIntentId == charge.PaymentIntentId);
+
+            if (charge.Status == "succeeded" && order != null) order.Status = OrderStatus.PaymentReceived;
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (!result)
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Something Happened with the stripe webhook",
+                    Status = 500
+                });
+            }
+            return new EmptyResult();
         }
     }
 }
