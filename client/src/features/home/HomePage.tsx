23de25a3bf8a9d1ab4767a20cf3b39f01e4eb853
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-11-01 23:19:12 +0000

    Landing Image Carousel in Homepage

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
+import Carousel from "nuka-carousel";
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import {Box, Button, Typography} from "@mui/material";
 
-interface HomePageProps {}
+const images = ["hero1", "hero2", "hero3"];
 
-export const HomePage: React.FC<HomePageProps> = ({}) => {
-  return <h1>Home Page</h1>;
+export const HomePage: React.FC = () => {
+    return (<>
+            <Carousel wrapAround
+                      renderCenterLeftControls={({previousSlide}) => (
+                          <Button onClick={previousSlide}>
+                              <ArrowBackIosNewRoundedIcon/>
+                          </Button>
+                      )} renderCenterRightControls={({nextSlide}) => (
+                <Button onClick={nextSlide}>
+                    <ArrowForwardIosIcon/>
+                </Button>
+            )}>
+                {images.map(image => (
+                    <img src={`../../../public/images/${image}.jpg`} alt={image}
+                         style={{display: "block", width: "100%", maxHeight: 700}}/>)
+                )}
+            </Carousel>
+            <Box sx={{pb: 4}} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+
+                <Typography variant="h1">Welcome to the store</Typography>
+            </Box>
+        </>
+    );
 };
