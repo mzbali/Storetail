784901807b94750306c999797589ce01bee80d8a
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2023-11-01 19:58:59 +0000

    Upgraded React and Mui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import "./app/layout/styles.css";
 import {Provider} from "react-redux";
 import {store} from "./app/store/configureStore";
 import {RouterProvider} from "react-router-dom";
 import router from "./app/router/Routes";
 
+const container = document.getElementById("root");
+const root = createRoot(container!);
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <RouterProvider router={router}/>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
